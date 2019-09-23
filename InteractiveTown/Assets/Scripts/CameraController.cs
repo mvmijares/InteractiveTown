f626028b5755be9ebc3e4bcb3864ca2da0217476
mvmijares <mvmijares@users.noreply.github.com>
2019-09-23 18:32:53 -0400

    Worked on player input, movement, and camera position. Working on rotating camera based on mouse cursor.

---
+++ InteractiveTown/Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject target;
+
+    public float cameraHeight;
+    public float cameraDistance;
+
+    public float smoothSpeed;
+
+    private Vector3 velocity;
+    
+    private void LateUpdate()
+    {
+        if (!target)
+            return;
+
+        HandleCameraInput();
+        HandleCameraPosition();
+        HandleCameraRotation();
+    }
+
+    private void HandleCameraInput()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        if(Input.GetKey(KeyCode.Escape))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    private void HandleCameraRotation()
+    {
+
+    }
+
+    private void HandleCameraPosition()
+    {
+
+        Vector3 offset = target.transform.forward * -cameraDistance;
+        Vector3 cameraPosition = target.transform.position + offset;
+        cameraPosition.y = cameraHeight;
+
+        transform.position = Vector3.SmoothDamp(transform.position, cameraPosition, ref velocity, smoothSpeed);
+
+        Debug.Log("Updating Camera Position.");
+    }
+}
---
+++ InteractiveTown/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Data
+    private int _xInput; // right/left
+    private int _yInput; // jump
+
+    private int _zInput; // forward/backward
+    public Vector3 movement;
+    public float moveSpeed;
+
+    #endregion
+
+    private void Update()
+    {
+        HandlePlayerInput();
+        HandlePlayerMovement();
+        
+    }
+
+    private void HandlePlayerMovement()
+    {
+        movement = new Vector3(_xInput, _yInput, _zInput);
+        transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandlePlayerInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+            _zInput = 1;
+        else if (Input.GetKey(KeyCode.S))
+            _zInput = -1;
+        else
+            _zInput = 0;
+
+        if (Input.GetKey(KeyCode.D))
+            _xInput = 1;
+        else if (Input.GetKey(KeyCode.A))
+            _xInput = -1;
+        else
+            _xInput = 0;
+    }
+}
